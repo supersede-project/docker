08ce636cebb1100ad12726e4b03ba4b72dc09eca
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-10-02 15:49:14 +0200

    MDM containers and composition

---
+++ mdm-frontend/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by snadal on 17/05/16.
+ */
+
+var config = {};
+
+config.PORT = 3000;
+config.METADATA_FRONTEND_URL = "http://localhost:"+config.PORT+"/";
+config.METADATA_DATA_LAYER_URL = "http://mdm-storage:8082/metadataStorage/";
+config.ONTO_MATCH_MERGE_URL = "http://mdm-storage:8082/ontoMatchMerge/";
+config.DEFAULT_NAMESPACE = "http://supersede/";
+
+module.exports = config;
