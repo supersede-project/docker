81f9cbcaaa0aeccdbfe00bb901df09c95398c4c0
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-10-17 11:47:58 +0200

    Fix MDM images and composition

--- mdm-frontend/config.js
+++ mdm-frontend/config.js
@@ -9,5 +9,6 @@ config.METADATA_FRONTEND_URL = "http://localhost:"+config.PORT+"/";
 config.METADATA_DATA_LAYER_URL = "http://mdm-storage:8082/metadataStorage/";
 config.ONTO_MATCH_MERGE_URL = "http://mdm-storage:8082/ontoMatchMerge/";
 config.DEFAULT_NAMESPACE = "http://supersede/";
+config.FILES_PATH = "/big_data/data_management/MetadataManagementSystem/MetadataFrontend/MDMfiles/";
 
 module.exports = config;
