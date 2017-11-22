9becde1e07df5f5f6a1fd2d114fd1b853030a286
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-11-22 13:07:14 +0100

    Updated ESB configuration. Docker image descriptors reconfigure to public usage

--- mysql/2-setup.sh
+++ mysql/2-setup.sh
@@ -2,17 +2,17 @@
 set -e
 set -x
 
-mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/orchestrator.sql
-mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/orchestrator_monitoring_test_dump.sql
-mysql -u supersede -p'supersede' feedback_repository < /supersede/repository.sql
+mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/docker_supersede_orchestrator_spring_clean.sql
+#mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/orchestrator_monitoring_test_dump.sql
+mysql -u supersede -p'supersede' feedback_repository < /supersede/docker_supersede_repository_spring_clean.sql
 mysql -u supersede -p'supersede' monitoring_orchestrator < /supersede/orchestrator.sql
 mysql -u supersede -p'supersede' model_repository_manager < /supersede/model_repository_deployment.sql
 
 #Apply patches
 
-mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/migrations/orchestrator/0002_active_flag_configuration_20161111.sql
-mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/migrations/orchestrator/0003_timestamp_millisecond_20161123.sql
+#mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/migrations/orchestrator/0002_active_flag_configuration_20161111.sql
+#mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/migrations/orchestrator/0003_timestamp_millisecond_20161123.sql
 
-mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/migrations/repository/0010_change_table_collations_20161111.sql
-mysql -u supersede -p'supersede' feedback_orchestrator < /supersede/migrations/repository/0011_change_timestamp_precision_20161123.sql
+#mysql -u supersede -p'supersede' feedback_repository < /supersede/migrations/repository/0010_change_table_collations_20161111.sql
+#mysql -u supersede -p'supersede' feedback_repository < /supersede/migrations/repository/0011_change_timestamp_precision_20161123.sql
 
