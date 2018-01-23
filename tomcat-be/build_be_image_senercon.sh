38775b5ee0c035933ec53539badd459ce10b3f5e
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-01-23 10:20:33 +0100

    Added scripts to install SUPERSEDE Evolution in Senercon

---
+++ tomcat-be/build_be_image_senercon.sh
@@ -0,0 +1,82 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage build_be_image.sh <supersede@supersede.es.atos.net password>"
+    exit 1
+fi
+
+echo -e "\nRetrieving SUPERSEDE Backend Services from CI/CD Server ...\n"
+PASSWORD=$1
+USER=supersede
+HOST=supersede.es.atos.net
+
+mkdir -p webapps
+
+#AppStore Monitor
+echo -e "\nRetrieving SUPERSEDE AppStore Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/appStore/build/libs/appStore.war
+FILE_OUT=./webapps/appStore.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#GooglePlay Monitor
+echo -e "\nRetrieving SUPERSEDE GooglePlay Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/googlePlay/build/libs/googlePlay.war
+FILE_OUT=./webapps/googlePlay.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Twitter Monitor
+echo -e "\nRetrieving SUPERSEDE Twitter Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/twitter/build/libs/twitterAPI.war
+FILE_OUT=./webapps/twitterAPI.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#HTTP Monitor
+echo -e "\nRetrieving SUPERSEDE HTTP Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/HttpMonitoring/build/libs/HttpMonitoring.war
+FILE_OUT=./webapps/HttpMonitoring.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#JsonLogs Monitor
+echo -e "\nRetrieving SUPERSEDE JsonLogs Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/JsonLogs/build/libs/JsonLogs.war
+FILE_OUT=./webapps/JsonLogs.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#PrjMonitoringUserEvents Monitor
+echo -e "\nRetrieving SUPERSEDE PrjMonitoringUserEvents Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/MonitoringUserEvents/PrjMonitoringUserEvents/target/PrjMonitoringUserEvents-0.0.1-SNAPSHOT.war
+FILE_OUT=./webapps/PrjMonitoringUserEvents.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#ConfigurationFileHTML Monitor
+echo -e "\nRetrieving SUPERSEDE ConfigurationFileHTML Monitor ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitors/ConfigurationFileHTMLMonitor/target/ConfigurationFileHTMLMonitor.war
+FILE_OUT=./webapps/ConfigurationFileHTMLMonitor.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Monitoring Manager
+echo -e "\nRetrieving SUPERSEDE Monitoring Manager ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/MonitorManagerAndMonitors/workspace/monitormanager/build/libs/monitormanager-0.1.0.war
+FILE_OUT=./webapps/monitormanager.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Feedback Repository 
+echo -e "\nRetrieving SUPERSEDE Feedback Repository ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Feedback\ Repository/workspace/repository/target/feedback_repository.war
+FILE_OUT=./webapps/feedback_repository.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Orchestrator 
+echo -e "\nRetrieving SUPERSEDE Orchestrator ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Feedback\ Orchestrator/workspace/orchestrator/target/orchestrator.war
+FILE_OUT=./webapps/orchestrator.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Replan Optimizer 
+echo -e "\nRetrieving SUPERSEDE Replan Optimizer ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Replan\ Optimizer/workspace/build/libs/replan_optimizer-0.0.1.war
+FILE_OUT=./webapps/replan_optimizer.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+
+echo -e "\nBuilding SUPERSEDE Backend Services Image ...\n"
+#docker build -t supersede/be -f Dockerfile_Senercon .
+
---
+++ tomcat-fe/build_fe_image_senercon.sh
@@ -0,0 +1,46 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage build_fe_image.sh <supersede@supersede.es.atos.net password>"
+    exit 1
+fi
+
+echo -e "\nRetrieving SUPERSEDE Frontend Services from CI/CD Server ...\n"
+PASSWORD=$1
+USER=supersede
+HOST=supersede.es.atos.net
+
+mkdir -p webapps
+
+#Frontend
+echo -e "\nRetrieving SUPERSEDE Frontend ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Supersede\ FE/workspace/applications/supersede-frontend/build/libs/supersede-frontend-0.2.1-SNAPSHOT.war
+FILE_OUT=./webapps/ROOT.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+
+#Copy admin-user-manager-app.war in $TOMCAT/webapps/
+echo -e "\nRetrieving SUPERSEDE Frontend Admin User Manager...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Supersede\ FE/workspace/applications/admin-user-manager-app/build/libs/admin-user-manager-app-0.2.1-SNAPSHOT.war
+FILE_OUT=./webapps/admin-user-manager-app.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Copy configuration-tools.war in $TOMCAT/webapps/
+echo -e "\nRetrieving SUPERSEDE Frontend Configuration Tools ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Supersede\ FE/workspace/applications/configuration-tools/build/libs/configuration-tools-0.0.1-SNAPSHOT.war
+FILE_OUT=./webapps/configuration-tools.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Copy supersede-dm-app.war in $TOMCAT/webapps/
+echo -e "\nRetrieving SUPERSEDE Frontend DM App ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/DM\ Game/workspace/applications/supersede-dm-app/build/libs/dm-app-0.0.2-M18.war
+FILE_OUT=./webapps/supersede-dm-app.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+#Replan Dashboard
+echo -e "\nRetrieving SUPERSEDE Frontend Replan Dashboard ...\n"
+FILE_IN=/opt/jenkins/apps/jenkins/jenkins_home/jobs/Replan\ Dashboard/workspace/release-planner-app/build/libs/release-planner-app-0.2-SNAPSHOT.war
+FILE_OUT=./webapps/release-planner-app.war
+sshpass -p "$PASSWORD" scp $USER@$HOST:"\"$FILE_IN\"" $FILE_OUT
+
+echo -e "\nBuilding SUPERSEDE Frontend Services Image ...\n"
+docker build -t supersede/fe -f Dockerfile_Senercon .
+
